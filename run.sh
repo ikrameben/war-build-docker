5857cb64effae568eb4d39f6ecc0192c1973bc9c
ikrameben <150833022+ikrameben@users.noreply.github.com>
2024-01-21 14:54:11 +0100

    Update run.sh
    change port -p 8081:8080 to 8181:81

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -d --name webapp -p 8081:8080 \
+docker run -d --name webapp -p 8181:81 \
 -v $(pwd)/target/hello-world-war-1.0.0.war:/usr/local/tomcat/webapps/ROOT.war \
 -v $(pwd)/target/hello-world-war-1.0.0/:/usr/local/tomcat/webapps/ROOT \
 tomcat
